46604504cbd89baf28cd9a0c0979505f39fb5378
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-03-10 13:02:17 +0200

    go react

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+export function Section({title}){
+    return <div>{title  &&  <h1>{title}</h1>}
+       
+    </div>
+};
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+};
